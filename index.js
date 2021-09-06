892096a5f2ecbba0a273b97095fd28459fc7d4b2
LeeProgrammer-Sub <jaeho010718+Sub@gmail.com>
2021-09-06 14:41:16 +0900

    basic_settings

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import 'react-native-gesture-handler';
 import { registerRootComponent } from 'expo';
 
-import App from './App';
+import App from './app/App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+
+/*
+import React from 'react'
+import {AppRegistry} from 'react-native'
+import App from './app/App'
+
+const TodoApp = () => <App/>
+
+AppRegistry.registerComponent('Todo',()=>TodoApp)
+*/
